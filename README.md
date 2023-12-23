c69d91e6359892fb146b37fb129343ca85174348
Vince Gabriel Flores <vincegabriel.flores@gmail.com>
2023-12-22 19:26:33 -0500

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+> Animated Rings with only CSS
+
+## Preview
+
+![Alt text](image-4.png)
---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>animated rings </h2>
+      </div>
+      <div className="circle-container flex-center">
+        <div className="circle-sub-container flex-center">
+          <div className="circle flex-center">
+            <div className="circle2 flex-center">
+              <div className="circle3 flex-center">
+                <div className="circle4"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
